addfc5e4659f32bd854ca828338fdd46d844192a
Nika Nyrkova <nikanyrkova@yandex.ru>
2021-05-07 19:13:23 +0300

    nyrk

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-import './styles.scss';
+const btn = document.querySelector('.btn');
+const title = document.querySelector('.hh');
+
+btn.addEventListener('click', () => {
+    title.textContent = 'hello'
+})
+
